985c2c74b2d0656244e7040f6ea142310378d0e7
bindeali <56399637+bindeali@users.noreply.github.com>
2022-05-20 06:59:09 +0200

    18.4.

--- src/test/java/cvut/alice/ottrmanager/OttrManagerApplicationTests.java
+++ src/test/java/cvut/alice/ottrmanager/OttrManagerApplicationTests.java
@@ -1,9 +1,10 @@
 package cvut.alice.ottrmanager;
 
+import cvut.alice.ottrmanager.main.OttrManagerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OttrManagerApplication.class)
 class OttrManagerApplicationTests {
 
     @Test
